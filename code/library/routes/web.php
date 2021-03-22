1eb5e1af52369ab394843fca904018ea7caeabfb
Sanakoev Anton <totum42@gmail.com>
2021-03-22 16:59:54 +0300

    App init

---
+++ code/library/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('db_test', function () {
+    //phpinfo();
+    $users = DB::select('select * from user where id >= ?', [1]);
+
+    var_dump($users);
+});
